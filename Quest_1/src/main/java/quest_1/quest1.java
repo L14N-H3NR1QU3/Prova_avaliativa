2b224bb12a3f9dff65744277b191b7bb6c5e6599
Lian <lian.h.silva@aluno.senai.br>
2024-10-25 19:18:51 -0300

    Atividade 1

---
+++ Quest_1/src/main/java/quest_1/quest1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package quest_1;
+
+/**
+ *
+ * @author l.silva
+ */
+import java.util.Scanner;
+public class quest1 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        System.out.println("Digite quantos anos voce tem: ");
+        int anoNascido = s.nextInt();
+        System.out.println("Digite quantos meses voce tem de idade: (des do seu ultimo aniversario) ");
+        int mesNascido = s.nextInt();
+        System.out.println("Digite quantos dias voce tem de idade: (des do seu ultimo aniversario)");
+        int diaNascido = s.nextInt();
+        int diasVivos = 0;
+        diasVivos += anoNascido * 365;
+        diasVivos += mesNascido * 30;
+        diasVivos += diaNascido;
+        System.out.println("Voce tem des do seu ultimo aniversario "+diasVivos+" dias vividos");
+    }
+}
---
+++ Quest_2/src/main/java/com/mycompany/quest_2/Quest_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quest_2;
+
+/**
+ *
+ * @author l.silva
+ */
+public class Quest_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
